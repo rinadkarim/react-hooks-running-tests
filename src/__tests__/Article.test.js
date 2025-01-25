9e246622d649325164c966010e1c304eb3d45c96
rinad karim <rinadjobs@gmail.com>
2025-01-25 13:03:21 -0500

    running tests

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -7,5 +7,16 @@ import Article from "../components/Article";
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
-  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+  const element = screen.queryByText("please pass this test");
+
+  screen.debug(element);
+
+  expect(element).toBeInTheDocument();
+
+  // to debug entire component use this
+  //  screen.debug();
+
+  // expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+// });
+
 });
